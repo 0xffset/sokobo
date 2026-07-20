42a544d820dd3599a8e70d71f8b2da9e18ce202d
0xffset <roggergarciadiaz@gmail.com>
2026-07-20 17:57:47 -0400

    feat(norm): Pending to definee norm test

---
+++ test/matrix/matrix_norm_test.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MATH
+#include <vector>
+
+#include "../../sokobo/source/include/matrix.h"
+#include "../../external/catch.hpp"
+using Catch::Matchers::Message;
+using Catch::Matchers::StartsWith;
+
+TEST_CASE("Norm of 1 x 1 non-zero matrix - Norm", "[norm]") {
+
+    Matrix<int> m = Matrix<int>(1,1);
+    std::vector<int> v = {5};
+    double result = m.norm(v);
+
+    int expected = 5;
+    CHECK(result == expected);
+}
